d062416b167bf048e6cc08bde3f4a6c7d5d8a3ea
ukhang dylan <ukhangmarma459@gmail.com>
2022-12-19 01:09:43 +0600

    adding progress bar

--- components/About.jsx
+++ components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
                         <span className="text-5xl text-[#0F172A] dark:text-white">Ukhang Marma</span>
                     </h2>
                     {/* <p className="text-[#1E293B] dark:text-white font-[500]">Full Stack Enginner / Comparative programmer</p> */}
-                    <p className="text-[#1E293B] dark:text-white font-[500]">FrontEnd Engineer</p>
+                    <p className="text-[#1E293B] dark:text-white font-[500]">Software Developer</p>
                 </div>
                 {/* Right */}
                 <div className="md:mx-0 mx-auto">
